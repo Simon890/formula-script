43df42b227983bb33929d95038ffe354ed7a1870
Simon890 <revellosimon890@gmail.com>
2024-04-09 17:47:00 -0300

    Added new exports in index.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { Interpreter } from "./src/Interpreter";
 import { Range, RangeHandlerFunction, RangeHandlerClassFunction } from "./src/types/range";
 import { RangeArray } from "./src/RangeArray";
 import { ObjectFormulaFunction } from "./src/types/formulaFunction";
+import { FormulaFunction } from "./src/FormulaFunction";
+import { Arguments } from "./src/Arguments";
 
 export const FormulaScript = Interpreter;
 export {
@@ -9,5 +11,7 @@ export {
     RangeHandlerClassFunction,
     RangeHandlerFunction,
     RangeArray,
-    ObjectFormulaFunction
+    ObjectFormulaFunction,
+    FormulaFunction,
+    Arguments,
 }
